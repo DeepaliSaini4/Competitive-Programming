6504d0e6d907508a784c9ae8650bdf44f4a1e263
Deepali Saini <deepalisa4444@gmail.com>
2024-12-19 11:53:08 +0000

    Detecting Cycle Using DFS

---
+++ Graphs/DFS/DetectCycle.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,m;
+vector<vector<int>>g;
+vector<int> vis;
+bool is_cycle_present = false;
+void dfs(int node, int par){
+    vis[node]=1;
+    for(auto v: g[node]){
+        if(!vis[v]){
+            dfs(v,node);
+        }
+        else if(v!=par) {
+            //cycle is present
+            is_cycle_present = 1;
+        }
+    }
+}
+signed main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    cin>>n>>m;
+    g.resize(n+1); 
+    for(int i;i<m;i++){
+        int a,b;
+        cin>>a>>b;
+        g[a].push_back(b);
+        g[b].push_back(a);
+    }
+    vis.assign(n+1,0);
+    for(int i=1;i<=n;i++){
+        if(!vis[i]){
+            dfs(i,0);
+        }
+    }
+ cout << (is_cycle_present ? "Cycle is present" : "No cycle detected") << endl;
+    return 0;
+}
