140dd4fbddee544ec02a98074239af760a060350
Deepali Saini <deepalisa4444@gmail.com>
2024-12-21 04:46:38 +0000

    Implemented topo Ordering Using Priority Queue in BFS

---
+++ Graphs/TopologicalOrdering/LexiographicallySmallestTopologicalOrdering.cpp
@@ -0,0 +1,47 @@
+//Lexiographically smallest topological ordering
+#include<bits/stdc++.h>
+using namespace std;
+vector<vector<int>>g;
+vector<int>indeg;
+vector<int> topo;
+int n,m;
+void kahn(){
+    priority_queue<int>q;
+    for(int i=1;i<=n;i++){
+        if(indeg[i]==0)q.push(-i);
+    }
+    while(!q.empty()){
+        int curr = -q.top();
+        q.pop();
+        topo.push_back(curr);
+        for(auto v:g[curr]){
+            indeg[v]--;
+            if(indeg[v]==0){
+                q.push(-v);
+            }
+        }
+        
+    }
+}
+int main(){
+
+    cin>>n>>m;
+    g.resize(n+1);
+    indeg.assign(n+1,0);
+    for(int i=0;i<m;i++){
+        int a,b;
+        cin>>a>>b;
+        g[a].push_back(b);
+        indeg[b]++;//as the edge is from a-->b
+    }
+    kahn();
+    if(topo.size()!=n){
+        cout<<"Cycle Detected!"<<endl;
+    } 
+    else{
+    for(auto v:topo){
+        cout<<v<<" ";
+       }
+    }
+}
+//small tweek in topological odering by bfs use a priority qeueue.
