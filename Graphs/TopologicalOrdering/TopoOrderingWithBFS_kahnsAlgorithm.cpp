e6d00e1cb011ef3be87c8502fac77766f3f8205f
Deepali Saini <deepalisa4444@gmail.com>
2024-12-30 10:58:23 +0000

    Kahns Algorithm

--- Graphs/TopologicalOrdering/TopoOrderingWithBFS_kahnsAlgorithm.cpp
+++ Graphs/TopologicalOrdering/TopoOrderingWithBFS_kahnsAlgorithm.cpp
@@ -1,14 +1,19 @@
 //Algorithm-->find the node which has indegree equal to zero add it to the initially empty topological ordering then remove it form the graph.
 #include<bits/stdc++.h>
 using namespace std;
+
+#define int long long
+
 vector<vector<int>>g;
 vector<int>indeg;
 vector<int> topo;
+
 int n,m;
+
 void kahn(){
     queue<int>q;
     for(int i=1;i<=n;i++){
-        if(indeg[i]==0)q.push(i);
+        if(indeg[i]==0) q.push(i);
     }
     while(!q.empty()){
         int curr = q.front();
@@ -23,17 +28,24 @@ void kahn(){
         
     }
 }
-int main(){
+
+signed main(){
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
 
     cin>>n>>m;
+
     g.resize(n+1);
     indeg.assign(n+1,0);
+
     for(int i=0;i<m;i++){
         int a,b;
         cin>>a>>b;
         g[a].push_back(b);
         indeg[b]++;//as the edge is from a-->b
     }
+
     kahn();
     if(topo.size()!=n){
         cout<<"Cycle Detected!"<<endl;
@@ -43,5 +55,7 @@ int main(){
         cout<<v<<" ";
        }
     }
+
+    return 0;
 }
-// if graph contains cycle then the size of the topological ordering will nevr be n
+// if graph contains cycle then the size of the topological ordering will never be n
