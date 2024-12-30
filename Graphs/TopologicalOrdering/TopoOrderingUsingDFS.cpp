895a4fb0744e11c4a8c40e6083b782a87385cf64
Deepali Saini <deepalisa4444@gmail.com>
2024-12-30 10:53:19 +0000

    Spacing modified

--- Graphs/TopologicalOrdering/TopoOrderingUsingDFS.cpp
+++ Graphs/TopologicalOrdering/TopoOrderingUsingDFS.cpp
@@ -1,9 +1,14 @@
 //Topological order is a linear arrangement of nodes in a directed graph where for every directed edge from node A to node B, A comes before B in the ordering.
 #include<bits/stdc++.h>
 using namespace std;
+
+#define int long long
+
 vector<vector<int>>g;
 vector<int>vis;
 vector<int>topo;
+int n,m;
+
 void dfs(int node){
     vis[node]=1;
     for(auto v:g[node]){
@@ -13,25 +18,32 @@ void dfs(int node){
     }
     topo.push_back(node);
 }
+
 signed main(){
-    int n,m;
+
     cin>>n>>m;
+
     g.resize(n+1);
     vis.assign(n+1,0);
+
     for(int i=0;i<m;i++){
         int a,b;
         cin>>a>>b;
         g[a].push_back(b);
     }
+
     for(int i=1;i<n;i++){
         if(!vis[i])
         dfs(i);
     }
+
     reverse(topo.begin(),topo.end());
     for(auto v:topo){
         cout<<v<<" ";
     }
+
+    return 0;
 }
 /*
-Every node that could be reached from the gievn node is reached through dfs and then stored first and later the array is reversed
+Every node that could be reached from the given node is reached through dfs and then stored first and later the array is reversed
 */
