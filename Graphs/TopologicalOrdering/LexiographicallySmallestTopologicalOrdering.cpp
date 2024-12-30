cb18f9a71e2bdecebaeba3d783167562a2947d39
Deepali Saini <deepalisa4444@gmail.com>
2024-12-30 11:05:59 +0000

    Spacing

--- Graphs/TopologicalOrdering/LexiographicallySmallestTopologicalOrdering.cpp
+++ Graphs/TopologicalOrdering/LexiographicallySmallestTopologicalOrdering.cpp
@@ -1,10 +1,15 @@
 //Lexiographically smallest topological ordering
 #include<bits/stdc++.h>
 using namespace std;
+
+#define int long long
+
 vector<vector<int>>g;
 vector<int>indeg;
 vector<int> topo;
+
 int n,m;
+
 void kahn(){
     priority_queue<int>q;
     for(int i=1;i<=n;i++){
@@ -23,21 +28,29 @@ void kahn(){
         
     }
 }
-int main(){
+signed main(){
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
 
     cin>>n>>m;
+
     g.resize(n+1);
     indeg.assign(n+1,0);
+
     for(int i=0;i<m;i++){
         int a,b;
         cin>>a>>b;
         g[a].push_back(b);
         indeg[b]++;//as the edge is from a-->b
     }
+
     kahn();
+
     if(topo.size()!=n){
         cout<<"Cycle Detected!"<<endl;
     } 
+    
     else{
     for(auto v:topo){
         cout<<v<<" ";
