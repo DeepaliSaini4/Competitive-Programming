b2231ee46eb6eb4789c2a8ae273594676f716ee7
Deepali Saini <deepalisa4444@gmail.com>
2025-01-12 15:23:31 +0530

    Update DZYLovesSequences.cpp

--- Dynamic_Programming/DP Form 2 Ending at i/DZYLovesSequences.cpp
+++ Dynamic_Programming/DP Form 2 Ending at i/DZYLovesSequences.cpp
@@ -1,4 +1,4 @@
-/*DZY has a sequence a, consisting of n integers.
+/*D-ZY has a sequence a, consisting of n integers.
 
 We'll call a sequence ai, ai + 1, ..., aj (1 ≤ i ≤ j ≤ n) a subsegment of the sequence a. The value (j - i + 1) denotes the length of the subsegment.
 Your task is to find the longest subsegment of a, such that it is possible to change at most one number (change one number to any integer you want) from the subsegment to make the subsegment strictly increasing.
