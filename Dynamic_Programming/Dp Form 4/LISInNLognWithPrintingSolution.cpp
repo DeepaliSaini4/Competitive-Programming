3a1b7c4ad8b0de0f309591c981fe8f4321d0e4ea
Deepali Saini <deepalisa4444@gmail.com>
2025-01-18 01:23:01 +0000

    Added second approach

--- Dynamic_Programming/Dp Form 4/LISInNLognWithPrintingSolution.cpp
+++ Dynamic_Programming/Dp Form 4/LISInNLognWithPrintingSolution.cpp
@@ -3,6 +3,7 @@
     10
     1 5 7 10 9 6 7 9 2 3
 */
+//------------------> ZENITH APPROACH
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -45,6 +46,51 @@ void solver(){
     
 }
 
+signed main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    solver();
+    return 0;
+}
+//-------------------------> KING APPROACH 
+#include<bits/stdc++.h>
+using namespace std;
+
+void solver(){
+    int n;
+    cin>>n;
+    int arr[n];
+    vector<int>par;
+    int maxi = -1;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+        maxi = max(maxi, arr[i]);
+    }
+    par.assign(maxi+1, 0);
+    
+    vector<int>lis;
+    for(int i=0;i<n;i++){
+        if(lis.empty() || lis.back()<arr[i]){
+            par[arr[i]] = lis.empty() ? -1 : lis.back();
+            lis.push_back(arr[i]);
+        } else {
+            auto it = lower_bound(lis.begin(),lis.end(),arr[i]);
+            *it  =arr[i];
+            par[arr[i]] = *(--it);
+        }
+    }
+        
+    vector<int> fin_lis;
+    fin_lis.push_back(lis.back());
+    for(int i=lis.back(); i>=0; ) {
+        if(par[i] == -1) break;
+        fin_lis.push_back(par[i]);
+        i = par[i];
+    }
+    
+    for(int i=fin_lis.size()-1; i>=0; i--) cout << fin_lis[i] << " ";
+}
+    
 signed main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
