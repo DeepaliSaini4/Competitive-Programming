f3748f9edfec2b93de54c19155dac6b841efc91c
Deepali Saini <deepalisa4444@gmail.com>
2025-02-08 23:30:58 +0530

    yet to be coded

---
+++ PatternBuilding/AnotherPyramidPattern.cpp
@@ -0,0 +1,29 @@
+/*
+Description
+For a given integer N, print the pattern as shown in the examples below.
+
+Input Format
+The first line of the input contains one integer T - the number of test cases. Then T test cases follow.The first line of each test case contains the integer N.
+
+Output Format
+For each test case, print the desired pattern. Add an empty line after each test case.
+
+Constraints
+1≤T≤10
+1≤N≤10^2
+
+Sample Input 1
+2
+3
+4
+
+Sample Output 1
+1     
+  1 2 1   
+1 2 3 2 1 
+
+      1       
+    1 2 1     
+  1 2 3 2 1   
+1 2 3 4 3 2 1
+*/
