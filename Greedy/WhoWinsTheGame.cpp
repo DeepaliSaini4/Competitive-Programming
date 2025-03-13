0dc3000e8e757216f44ec979ce9aad1bb9451404
Deepali Saini <deepalisa4444@gmail.com>
2025-03-13 15:03:22 +0530

    Pick and block the max choice simultaneously

---
+++ Greedy/WhoWinsTheGame.cpp
@@ -0,0 +1,95 @@
+/*
+Description
+Alice and Bob are playing a new game, which is very interesting and fun. The game is as follows:
+The game starts with two n-sized integer arrays, A and B, and is played by two players, P1 and P2.
+The players move in alternating turns, with P1 always moving first. During each move, the current player must choose an integer, i, such that 0≤i≤n−1.
+If the current player is P1, then P1 receives Ai points.
+If the current player is P2, then P2 receives Bi points.
+Each value of i can be chosen only once. That is, if a value of 
+i is already chosen by some player, none of the players can re-use it. So, the game always ends after n moves.
+The player with the maximum number of points wins.
+The arrays A and B are accessible to both the players P1 and P2 . So the players make an optimal move at every turn.
+Given the values of n, A, and B, can you determine the outcome of the game? 
+P1 is Alice, and P2is Bob.
+Print ‘Alice’ if Alice will win, 'Bob' if Bob will win, or 'Tie' if they will tie. Assume both players always move optimally.
+
+Output Format
+For each test case, print one of the following predicted outcomes of the game on a new line:
+Print 'Alice' if Alice will win.
+Print 'Bob' if Bob will win.
+Print 'Tie' if the two players will tie.
+
+Sample Input 1
+3
+3
+1 3 4
+5 3 1
+2
+1 1
+1 1
+2
+2 2
+3 3
+
+Sample Output 1
+Alice
+Tie
+Bob
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+void solver(){
+    int n;
+    cin>>n;
+
+    int a[n];
+    int b[n];
+
+    for(int i=0;i<n;i++) cin>>a[i];
+    
+
+    for(int i=0;i<n;i++) cin>>b[i];
+     
+
+    //optimal choice is not just picking the max element but also blocking the maximum element for the other player
+
+    vector<pair<int,pair<int,int>>>sum_ab;
+    for(int i=0;i<n;i++){
+        int sum = a[i] + b[i];
+        sum_ab.push_back(make_pair(sum,make_pair(a[i],b[i])));
+    }
+
+    //sorting in the descending order
+    sort(sum_ab.begin(),sum_ab.end(),[](pair<int,pair<int,int>> a,pair<int,pair<int,int>> b){
+        return a.first > b.first;
+    });
+
+    int score_a = 0;
+    int score_b = 0;
+
+    for(int i=0;i<n;i++){
+        if(i%2==0) score_a += sum_ab[i].second.first;
+        else score_b += sum_ab[i].second.second;
+    }
+
+    //result printing
+    if(score_a>score_b)cout<<"Alice"<<'\n';
+    else if(score_a<score_b)cout<<"Bob"<<'\n';
+    else cout<<"Tie"<<'\n';
+
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+
+    int t;
+    cin>>t;
+
+    while(t--)solver();
+
+    return 0;
+}
