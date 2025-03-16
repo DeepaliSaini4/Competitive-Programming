044db439751d22a016a585c026190a8a503918e7
Deepali Saini <deepalisa4444@gmail.com>
2025-03-16 18:50:28 +0530

    arr[mid] == 1

---
+++ BinarySearch/FirstOccurrenceOfOne.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long 
+
+int arr[100100];
+
+bool check(int mid){
+    return arr[mid] == 1;
+}
+
+void solver(){
+    int n;
+    cin>>n;
+    
+    for(int i=0;i<n;i++) cin>>arr[i];
+    
+    int lo = 0;
+    int hi = n-1;
+    int ans = -1;
+    
+    while(lo<=hi){
+        int mid = lo + ((hi-lo)/2);
+        if(check(mid)==1){
+            ans = mid;
+            hi = mid-1;
+        }
+        else lo = mid+1;
+        }
+        
+    cout<<ans<<'\n';
+}
+
+
+signed main(){
+    
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    
+    int t;
+    cin>>t;
+    
+    while(t--) solver();
+    
+    return 0;
+}
