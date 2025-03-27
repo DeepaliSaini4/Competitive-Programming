a1aa94732069bf855abd4fecfc19bd738183ac7b
Deepali Saini <deepalisa4444@gmail.com>
2025-03-27 17:31:46 +0530

    yet to be coded

---
+++ C++/C++ Landmines - 2/PatternProblem.cpp
@@ -0,0 +1,22 @@
+/*
+Using the characters: . (dot), * (asterisk), / (slash), and \ (backslash), print a grid-like pattern as shown in the sample test cases.
+The diagonal of the first square of the grid is directed towards the down-right corner; use the \ (backslash) character to print it.
+The next diagonal will be directed towards the upper-right corner; use the / (slash) character to print it.
+
+Sample:
+1
+3 4 3
+*****************
+*\..*../*\..*../*
+*.\.*./.*.\.*./.*
+*..\*/..*..\*/..*
+*****************
+*../*\..*../*\..*
+*./.*.\.*./.*.\.*
+*/..*..\*/..*..\*
+*****************
+*\..*../*\..*../*
+*.\.*./.*.\.*./.*
+*..\*/..*..\*/..*
+*****************
+*/
