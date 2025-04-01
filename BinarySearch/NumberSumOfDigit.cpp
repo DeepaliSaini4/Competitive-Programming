b738bdf3219f68ff662dea551bc220ef5cba49c3
Deepali Saini <deepalisa4444@gmail.com>
2025-04-01 11:53:23 +0530

    Create NumberSumOfDigit.cpp

---
+++ BinarySearch/NumberSumOfDigit.cpp
@@ -0,0 +1,4 @@
+/*
+You are given two integers N and S.
+Find the number of positive integers X less than equal to N, such that the difference between X and the sum of its digits (in decimal representation) is not less than S.
+*/
