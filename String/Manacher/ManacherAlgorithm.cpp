32d831f33c666b6cf72f2bc74ca1db82f1044b3a
Deepali Saini <deepalisa4444@gmail.com>
2025-04-12 01:09:38 +0530

    o(n)

--- String/Manacher/ManacherAlgorithm.cpp
+++ String/Manacher/ManacherAlgorithm.cpp
@@ -4,19 +4,22 @@ using namespace std;
 #define int long long 
 
 struct manacher{
-    vector<int>p;
+    vector<int>p;// Stores the radius of the longest palindrome centered at each position.
    
     void run_manacher(string s){
         int n = s.length();
         cout<<s<<'\n';
-        p.assign(n,1);
-        int l = 1;
+        p.assign(n,1);// Start with radius 1 for all positions (each character is its own palindrome).
+        int l = 1;// Tracks the farthest-reaching palindrome [l, r].
         int r = 1;
         for(int i=1;i<n;i++){
+            // Use mirror symmetry to avoid redundant checks.
             p[i] = max(0LL,min(r-i,p[l+r-i]));
+             // Expand the palindrome centered at 'i'.
             while(i+p[i]<n && i-p[i]>=0 && s[i+p[i]]==s[i-p[i]]){
                 p[i]++;
             }
+             // Update the farthest-reaching palindrome
             if(i+p[i]>r){
                 l = i-p[i];
                 r = i + p[i];
@@ -27,19 +30,21 @@ struct manacher{
         }
         cout<<"\n";
     }
-    
+    // Preprocessing the String
     void build(string s){
         string t;
         for(auto v:s){
-            t+= string("#")+v;
+            t+= string("#")+v;// Insert '#' between characters. Example: "abc" → "#a#b#c"
         }
-        run_manacher(t+"#");
+        run_manacher(t+"#");// Add a final '#'
     }
     //abbabba
     int getlongest(int cen,bool odd){
-        int pos = 2*cen+1+(!odd);
-        return p[pos]-1;
+        int pos = 2*cen+1+(!odd);// Convert original string's center to preprocessed position.
+        return p[pos]-1;// Adjust for the '#' characters.
+}
     }
+//Check If Substring is a Palindrome
     bool checkpalin(int l,int r){
         if((r-l+1)<=getlongest(l+r/2,l%2==r%2)){ return 1;
         }else{
