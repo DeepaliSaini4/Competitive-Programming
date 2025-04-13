1761aa31b323b707c45a98908d515de888bfe72d
Deepali Saini <deepalisa4444@gmail.com>
2025-04-13 19:21:26 +0530

    indexing logic in final substr

---
+++ String/Manacher/LongestPalindrome.cpp
@@ -0,0 +1,68 @@
+/*
+Description
+Given a string of length n, your task is to determine the longest palindromic substring of the string. For example, the longest palindrome in "algoazzaa" is "azza".
+If there are multiple longest palindromic substrings, Output the one which comes first in the string.
+
+Input Format
+The first line contains T - the number of test cases.
+The first line of each test case contains a string of length n. Each character is one of a–z.
+
+Output Format
+For each test case, Output the Longest Palindromic Substring.
+If there are multiple longest palindromic substrings, Output the one which comes first in the string.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long 
+
+void solver(){
+    string s;
+    cin >> s;
+
+    // Preprocess the string with separators
+    string t = "#";
+    for (char ch : s) {
+        t += ch;
+        t += "#";
+    }
+
+    int n = t.size();
+    vector<int> p(n, 1);
+
+    int l = 1, r = 1;
+    for (int i = 1; i < n; i++) {
+       
+            p[i] = min(r - i, p[l + r - i]);
+            
+        while (i - p[i] >= 0 && i + p[i] < n && t[i - p[i]] == t[i + p[i]]) {
+            p[i]++;
+        }
+        if (i + p[i] > r) {
+            l = i - p[i];
+            r = i + p[i];
+        }
+    }
+
+    // Find the longest palindrome
+    int max_len = 0, center = 0;
+    for (int i = 1; i < n; i++) {
+        if (p[i] > max_len) {
+            max_len = p[i];
+            center = i;
+        }
+    }
+
+    // Extract longest palindromic substring from original string
+    int start = (((center - (max_len-1)) )/2);
+    cout << s.substr(start, max_len-1) << '\n';
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int t;
+    cin >> t;
+    while(t--) solver();
+    return 0;
+}
