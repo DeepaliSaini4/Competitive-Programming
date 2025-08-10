256885dc424b46c8240835db0efb435d517f2d59
Deepali Saini <deepalisa4444@gmail.com>
2025-08-10 16:32:23 +0530

    Monotonic deq for maintaining decreasing order of incoming elements

--- Striver/SlidingWindow/SlidingWindowMax.cpp
+++ Striver/SlidingWindow/SlidingWindowMax.cpp
@@ -27,3 +27,27 @@ public:
         return ans;
     }
 };
+
+//DEQUE IMPLEMENTATION - O(N)
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque <int> dq;
+        vector<int> ans;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+
+            while (!dq.empty() &&  nums[dq.back()] < nums[i]) {
+                dq.pop_back();
+            }
+            dq.push_back(i);
+
+            if(i-k>=0 && i-k==dq.front()) dq.pop_front();
+
+            if (i >= k-1) {
+                ans.push_back(nums[dq.front()]);
+            }
+        }
+        return ans;
+    }
+};
