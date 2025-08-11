b16b40be3eb45b6e08636784389e69f6a4737f34
Deepali Saini <deepalisa4444@gmail.com>
2025-08-11 17:43:19 +0530

    o(n)

---
+++ Striver/Stack/MonotonicStack/901_Online_Stock_Span.cpp
@@ -0,0 +1,21 @@
+class StockSpanner {
+public:
+    stack<pair<int, int>> s;
+    StockSpanner() { s = stack<pair<int, int>>(); }
+
+    int next(int price) {
+        int cnt = 1;
+        while (!s.empty() && s.top().first <= price) {
+            cnt += s.top().second;
+            s.pop();
+        }
+        s.push({price, cnt});
+        return cnt;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
