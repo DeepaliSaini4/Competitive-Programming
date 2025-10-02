ba9d0a8f87796c48b9da0b834a2443e69cdf97ca
Deepali Saini <deepalisa4444@gmail.com>
2025-10-02 23:13:19 +0530

    Create ValidParenthesis.cpp

---
+++ Recursion/LCCMFrameworkPractice/ValidParenthesis.cpp
@@ -0,0 +1,9 @@
+/* Q- Valid parenthesis --> some validity checking parameter like depth?--> dp constraint?
+      All possible combos --> recursion + backtrack if invalid combo is being formed
+      //backtracking but how to form all possible pairs
+      1)by keeping at one given time two choice
+      2)by first performing one choice and then performing the second 
+      ---> need to choose one out of these two approaches to build on the ans
+      lexicographical order --> sort? ya store while generating in such a way that formed and stored in lexico order on its own
+      backtracking needed?? -> yes buidling from scratch once again every time is going to consume so much of more unecessary computational time for us
+*/
